a8e30a3ea80a4e0c91e92d990128742abfcfa72c
namexi <406633257@qq.com>
2018-09-26 17:50:07 +0800

    01-构建环境

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<style>
+
+</style>
+
+<template>
+    <div>
+        <jtodo-header></jtodo-header>
+    </div>
+</template>
+
+<script>
+import JtodoHeader from './component/header.vue'
+export default {
+    name: 'App',
+    components: {
+        JtodoHeader
+    }
+}
+</script>
---
+++ src/component/header.vue
@@ -0,0 +1,15 @@
+<style lang="scss">
+    .j-header {
+        color:red
+    }
+</style>
+
+<template>
+    <div class="j-header">header</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
